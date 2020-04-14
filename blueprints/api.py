00ddb76a69526b39740a0a24646304ee6507f150
Jack Greenberg <j.lester.greenberg@gmail.com>
2020-04-13 17:13:39 -0700

    Add structure for API
    This commit also fixes import statements that broke with the creation of
    /__init__.py.

---
+++ blueprints/api.py
@@ -0,0 +1,17 @@
+from flask import jsonify, request
+import json
+from flask.views import MethodView
+from datetime import datetime
+
+class API(MethodView):
+    def get(self, id):
+        return None
+
+    def post(self, id):
+        return None
+
+    def put(self, id):
+        return None
+
+    def delete(self, id):
+        return None
