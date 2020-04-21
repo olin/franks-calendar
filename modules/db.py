3e8c06ba6419da1b20b8e3a461a19f41654a7af0
Jack Greenberg <j.lester.greenberg@gmail.com>
2020-04-20 17:22:45 -0700

    Update db.py with functions for checking hashes

--- modules/db.py
+++ modules/db.py
@@ -1,21 +1,45 @@
-from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
+from pymongo import MongoClient
+import yaml
+from os.path import dirname, join
+from urllib.request import urlopen
+from icalendar import Calendar, Event
+import os
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://jackg:eminem answered primarily charity@192.168.86.75:3306/franks_calendar"
-db = SQLAlchemy(app)
+file = open(join(dirname(__file__), "../private/config.yml"))
 
-def wrap_db(app):
-    db.init_app(app)
+data = yaml.load(file.read(), Loader=yaml.SafeLoader)['db']
 
-class Calendar(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    url = db.Column(db.String(500))
+client = MongoClient(
+        data['host'] + ':' + str(data['port']),
+        username=data['user'],
+        password=data['password'],
+        authSource=data['dbName']
+)
 
-class Event(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    date = db.Column(db.DateTime)
-    tags = db.Column(db.String(255))
+db = client[data['dbName']]
 
+def check_for_changes():
+    url = "https://calendar.google.com/calendar/ical/oc7b32uiqmg7f063or4n1tmdkk%40group.calendar.google.com/private-9c9790cd2613d41bc9c3d1a51831d430/basic.ics"
+    ical = urlopen(url).read()
+    cal = Calendar.from_ical(ical)
+    print(hash(ical))
+
+    n = db.hashes.find_one({'name': 'latest'})
+    print(n['hash'])
+
+    if (hash(ical) != n['hash']):
+        db.hashes.find_one_and_update({'_id': n['_id']}, {
+            "$set": {"hash": hash(ical)}
+        })
+        pid = fork()
+        if pid == 0:
+            update_database()
+            os._exit()
+        else:
+            return 1
+    else:
+        return 0
+
+if __name__ == "__main__":
+    check_for_changes()
