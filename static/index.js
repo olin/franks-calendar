1aff147ea24b82fad151c013cccd600d8112e07f
Jack Greenberg <j.lester.greenberg@gmail.com>
2020-04-26 16:05:55 -0700

    Add recurring rule to React app

--- static/index.js
+++ static/index.js
@@ -7,11 +7,21 @@ import './index.scss';
 // Calendar
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import rrulePlugin from '@fullcalendar/rrule';
 
 import client from './api';
 import { ErrorBoundary } from './errorboundary';
 
 export const AppContext = React.createContext({});
+
+function clean_event_list(events) {
+    for (var i = 0; i < events.length; i++) {
+        console.log(events[i]);
+        events[i]['start'] = new Date(events[i]['start']['$date']);
+        events[i]['end'] = new Date(events[i]['end']['$date']);
+    }
+    return events
+}
 
 class App extends React.Component {
     constructor(props) {
@@ -21,14 +31,20 @@ class App extends React.Component {
             events: null,
             ready: false,
         }
+
+        this.eventClick = this.eventClick.bind(this);
+    }
+
+    eventClick(e) {
+        console.log(e);
     }
 
     componentDidMount() {
         client.get('/api/events')
         .then(res => {
-            console.log(res)
+            // console.log(res)
             this.setState({
-                events: res.data,
+                events: clean_event_list(res.data),
                 ready: true
             })
         })
@@ -38,9 +54,15 @@ class App extends React.Component {
     }
 
     render() {
+        // console.log(this.state.events)
         return (
             <>
-                <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin ]} />
+                <FullCalendar
+                    defaultView="dayGridMonth"
+                    plugins={[ dayGridPlugin, rrulePlugin ]}
+                    events={this.state.events}
+                    eventClick={this.eventClick}
+                />
             </>
         );
     };
