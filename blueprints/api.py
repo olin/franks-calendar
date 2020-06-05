fb070ad586321ea8895f010c50b904c51a34328e
Jack Greenberg <j.lester.greenberg@gmail.com>
2020-06-04 20:51:06 -0700

    Fill out API blueprint

--- blueprints/api.py
+++ blueprints/api.py
@@ -1,9 +1,11 @@
-from flask import jsonify, request
+from flask import jsonify, request, jsonify
 import json
 from flask.views import MethodView
 from datetime import datetime
 from bson import json_util
+from bson.objectid import ObjectId
 from modules.db import db
+from datetime import datetime, timezone
 
 class API(MethodView):
     def get(self, id):
@@ -14,11 +16,30 @@ class API(MethodView):
                 ret.append(json.loads(json_util.dumps(doc)))
         return jsonify(ret)
 
-    def post(self, id):
-        return None
+    def post(self):
+        request_data = json.loads(request.get_data(as_text=True))
+
+        event = {
+            "_id": ObjectId(),
+            "title": request_data.get("title"),
+            "description": request_data.get("description"),
+            "location": request_data.get("location"),
+            "start": request_data.get("start"),
+            "end": request_data.get("end"),
+            "rrule": request_data.get("rrule"),
+            "last_edited": datetime.now(timezon.utc),
+        }
+        db.events.insert_one(event)
+        return jsonify(json.loads(json_util.dumps(event))), 201
 
     def put(self, id):
-        return None
+        request_data = json.loads(request.get_data(as_text=True))
+
+        db.posts.find_one_and_update({'_id': ObjectId(id)}, {
+            "$set": request_data,
+        }, return_document=ReturnDocument.AFTER)
+        return jsonify(return_document)
 
     def delete(self, id):
-        return None
+        db.content.delete_one({'_id': ObjectId(id)})
+        return jsonify("ObjectId(" + content_id + ") deleted."), 200
