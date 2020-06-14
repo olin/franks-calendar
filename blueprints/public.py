e9280464913f6cdfd142953b80352aaa3f458957
Kelly Yen <kellyyen4101@gmail.com>
2020-06-13 20:31:07 -0700

    made edits jack suggested from pull request

--- blueprints/public.py
+++ blueprints/public.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, request, send_file, make_response
 from modules.db import db
 from bson.objectid import ObjectId
-import json
 from icalendar import Calendar, Event
+from datetime import datetime
 
 public = Blueprint('public', __name__, template_folder='../templates', static_folder='../static/build/')
 
@@ -10,7 +10,7 @@ public = Blueprint('public', __name__, template_folder='../templates', static_fo
 def public_index():
     return render_template('home.html')
 
-@public.route('/p/<page>')
+@public.route('/<page>')
 def public_page(page):
     try:
         rendered_page = render_template("%s.html" % page)
@@ -26,16 +26,10 @@ def export_event(eventid):
         event_data = db.events.find_one({'_id' : ObjectId(eventid)})
         cal = Calendar()
         event = Event()
-        event['dtstart'] = convert_time(str(event_data['start']))
-        event['dtend'] = convert_time(str(event_data['end']))
+        event['dtstart'] = datetime.strftime(event_data['start'], '%Y%m%dT%H%M%S')
+        event['dtend'] = datetime.strftime(event_data['end'], '%Y%m%dT%H%M%S')
         event['summary'] = event_data['title']
         event['location'] = event_data['location']
         event['description'] = event_data['description']
         cal.add_component(event)
         return cal.to_ical()
-
-def convert_time(dt):
-    #converts date time obejct into icalendar readable start and end time
-    edited = ''.join(e for e in dt if e.isalnum())
-    result = edited[:8] + "T" +  edited[8:]
-    return result
