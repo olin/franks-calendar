01140bf005433ce5470a9d6d9d823d335e06b630
Julia (Jules) Brettle <57976312+julesbrettle@users.noreply.github.com>
2020-07-08 23:05:23 -0700

    Implement Edit Event page layout and styles

--- blueprints/public.py
+++ blueprints/public.py
@@ -120,3 +120,8 @@ def export_event(eventid):
         event["description"] = event_data["description"]
         cal.add_component(event)
         return cal.to_ical()
+
+@public.route("/test-edit", methods=["GET"])
+def test_edit_page():
+    form = EventForm()
+    return render_template("edit.html", form=form)
